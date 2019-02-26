c382e9fb30ef81ba036e8753a3818aa5d3e6ce61
Laksh Singla <lakshsingla@gmail.com>
2019-02-26 11:57:55 +0530

    Fetching data for the profshows signed by the user

--- src/components/MoneyDrawers/ProfshowDrawer.js
+++ src/components/MoneyDrawers/ProfshowDrawer.js
@@ -9,13 +9,29 @@ import {
   TableRow
 } from '@material-ui/core'
 import { connect } from 'react-redux'
+import request from 'request'
 
 import MoneyDrawerBase from './MoneyDrawerBase'
 import classes from './styles.module.scss'
+import * as api from '@/constants/api'
 
 class RecieveMoneyDrawer extends Component {
   constructor(props) {
     super(props)
+  }
+  componentWillMount() {
+    request({
+      method: 'GET',
+      url: api.GET_PROFSHOWS,
+      headers: {
+        'X-Wallet-Token': api.WALLET_TOKEN,
+        'Authorization': `JWT ${this.props.auth.JWT}`
+      }
+    }, function (error, response, body) {
+      console.log('Status:', response.statusCode);
+      console.log('Headers:', JSON.stringify(response.headers));
+      console.log('Response:', body);
+    });
   }
   render() {
     return (
@@ -30,18 +46,22 @@ class RecieveMoneyDrawer extends Component {
               marginBottom: "20px"
             }}>PROFSHOWS SIGNED</h3>
           <Table>
-            <TableRow>
-              <TableCell>Profshow</TableCell>
-              <TableCell align="right">Quantity</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>SEL</TableCell>
-              <TableCell align="right">2</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell>Guthrie Govan</TableCell>
-              <TableCell align="right">1</TableCell>
-            </TableRow>
+            <TableHead>
+              <TableRow>
+                <TableCell>Profshow</TableCell>
+                <TableCell align="right">Quantity</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              <TableRow>
+                <TableCell>SEL</TableCell>
+                <TableCell align="right">2</TableCell>
+              </TableRow>
+              <TableRow>
+                <TableCell>Guthrie Govan</TableCell>
+                <TableCell align="right">1</TableCell>
+              </TableRow>
+            </TableBody>
 
           </Table>
           {/* <QRCode value={this.props.userProfile.qrCode} /> */}
@@ -53,6 +73,7 @@ class RecieveMoneyDrawer extends Component {
 }
 
 const mapStateToProps = state => ({
-  userProfile: state.userProfile
+  auth: state.auth,
+  userProfile: state.userProfile,
 })
 export default connect(mapStateToProps, null)(RecieveMoneyDrawer)
--- src/constants/api.js
+++ src/constants/api.js
@@ -2,7 +2,7 @@ import { WALLET_TOKEN } from "@/.env"
 
 //Routes
 export const API_ROOT = "http://139.59.64.214/wallet"
-export const TICKET_MANAGER = "http://139.59.64.214/ticket-manager"
+export const TICKET_MANAGER = "http://139.59.64.214/tickets-manager"
 
 export const OUTSTATION_LOGIN = `${API_ROOT}/auth`
 export const GET_VENDORS = `${API_ROOT}/vendor/`
@@ -11,6 +11,8 @@ export const GET_ORDERS = `${API_ROOT}/orders/`
 export const PLACE_ORDER = `${API_ROOT}/orders/`
 
 export const TRANSFER_MONEY = `${API_ROOT}/monetary/transfer`
+
+export const GET_PROFSHOWS = `${TICKET_MANAGER}/tickets`
 
 //Wallet
 export { WALLET_TOKEN };
