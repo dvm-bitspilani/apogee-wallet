0d6428e96a5765c2a062406c471a88332f600ebf
Laksh Singla <lakshsingla@gmail.com>
2019-03-02 16:43:49 +0530

    OTP shown functionality created

--- src/components/Orders/index.js
+++ src/components/Orders/index.js
@@ -6,11 +6,13 @@ import {
   ExpansionPanel,
   ExpansionPanelDetails,
   ExpansionPanelSummary,
+  Chip,
+  Button,
   Table,
   TableRow,
   TableHead,
   TableBody,
-  TableCell
+  TableCell,
 } from '@material-ui/core'
 
 import {
@@ -21,6 +23,20 @@ import * as orders from "@/actionCreators/orders"
 import classes from './styles.module.scss'
 
 class Orders extends Component {
+  countOrders = shells => shells.map(shell => shell.orders).flat().length
+
+  setOtpTrue = orderNumber => {
+    const x = [...this.state.otpShown];
+    x[orderNumber] = true;
+    this.setState({ otpShown: x });
+  }
+
+  countOrderNumber = (i, j) => {
+    const ordersBefore = this.countOrders(this.props.orders.orders.slice(0, j))
+    const elem = ordersBefore + i;
+    return elem;
+  }
+
   CircularDiv = (props) => (
     <div style={{
       ...props,
@@ -30,83 +46,106 @@ class Orders extends Component {
     }}></div>
   )
 
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
   componentWillMount() {
     this.props.getOrders();
   }
 
+  componentWillReceiveProps(props) {
+    this.setState({
+      otpShown: new Array(this.countOrders(props.orders.orders)).fill(false)
+    })
+  }
+
+
   render() {
     const { CircularDiv } = this
     const { orders } = this.props.orders
+    const ReadyCode = 2;
+
     return (
       <>
         <Typography variant="h4">ORDERS</Typography>
 
-        <div style = {{ marginTop: '30px', width: '100%'}}>
+        <div style={{ marginTop: '30px', width: '100%' }}>
           {orders.map(
-            shell => shell.orders.map(order => (
-              <ExpansionPanel key={`${shell.id}-${order.id}`} style={{ width: "100%" }}>
-                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} >
-                  <div style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    width: "100%",
-                    justifyContent: "space-between"
-                  }}>
-                    <div style={{
-                      display: "flex",
-                      flexDirection: "column",
-                    }}>
-                      <Typography >{order.vendor.name}</Typography>
-                      <Typography
-                        fontWeight={300}
-                        variant="subtitle2"
-                        classes={{
-                          root: classes.timestampDisplay
-                        }}
-                      >
-                        {shell.timestamp}
-                      </Typography>
-                    </div>
-                    <div style={{
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center"
-                    }}>
-                      <Typography>{
-                        ["Pending", "Accepted", "Finished"][order.status]
-                      }</Typography>
+            (shell, j) => shell.orders.map((order, i) => {
+              return (
+                <ExpansionPanel key={`${shell.id}-${order.id}`} style={{ width: "100%" }}>
+                  <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />} >
+                    <div className={classes.orderSummaryContainer}>
+                      <div className={classes.orderNameDate}>
+                        <Typography >{order.vendor.name}</Typography>
+                        <Typography
+                          fontWeight={300}
+                          variant="subtitle2"
+                          classes={{
+                            root: classes.timestampDisplay
+                          }}
+                        >
+                          {shell.timestamp}
+                        </Typography>
+                      </div>
                       <div style={{
                         display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center"
+                        flexDirection: "row",
                       }}>
-                        {
-                          [<CircularDiv key="pending" backgroundColor="red" marginRight="4px" />,
-                            <CircularDiv key="accepted" backgroundColor="yellow" marginRight="4px" />,
-                            <CircularDiv key="finished" backgroundColor="green" />].splice(0, 1+order.status)
-                        }
+                        {/* {order.status === this.ReadyCode && <Chip label={order.status} />} */}
+                        {/* {order.status > this.ReadyCode && <Chip label={order.status} />} */}
+                        {order.status <= ReadyCode &&
+                          <Button
+                            variant="outlined"
+                            size="small"
+                            color="primary"
+                            disabled={this.state.otpShown[this.countOrderNumber(i, j)]}
+                            classes={{
+                              disabled: classes.otpDisableButton
+                            }}
+                            onClick={(e) => {
+                              e.stopPropagation();
+                              this.setOtpTrue(this.countOrderNumber(i, j));
+                            }}
+                          >
+                            {this.state.otpShown[this.countOrderNumber(i, j)] ? order.otp : "OTP"}
+                          </Button>}
+                        <div className={classes.orderStatus}>
+                          <Typography style={{ textAlign: "right" }}>{
+                            ["Pending", "Accepted", "Ready", "Finished", "Declined"][order.status]
+                          }</Typography>
+                          <div className={classes.orderStatusVis}>
+                            {
+                              [<CircularDiv key="pending" backgroundColor="red" marginRight="4px" />,
+                              <CircularDiv key="accepted" backgroundColor="yellow" marginRight="4px" />,
+                              <CircularDiv key="finished" backgroundColor="green" />].splice(0, 1 + order.status)
+                            }
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </ExpansionPanelSummary>
-                <ExpansionPanelDetails
-                  classes={{
-                    root: classes.expansionTable
-                  }}>
-                  <Table>
-                    <TableHead />
-                    <TableBody>
-                      {order.items.map(item => (
-                        <TableRow key={item.id}>
-                          <TableCell>{item.name}</TableCell>
-                          <TableCell align="right">{item.quantity}</TableCell>
-                        </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </ExpansionPanelDetails>
-              </ExpansionPanel>
-            ))
+                  </ExpansionPanelSummary>
+                  <ExpansionPanelDetails
+                    classes={{
+                      root: classes.expansionTable
+                    }}>
+                    <Table>
+                      <TableHead />
+                      <TableBody>
+                        {order.items.map(item => (
+                          <TableRow key={item.id}>
+                            <TableCell>{item.name}</TableCell>
+                            <TableCell align="right">{item.quantity}</TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </ExpansionPanelDetails>
+                </ExpansionPanel>
+              )
+            })
           ).flat()}
         </div>
       </>
